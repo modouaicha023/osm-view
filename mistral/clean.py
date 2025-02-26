4eef76459b9c8c5e2b804643e3f9497b146ef294
modouaicha023 <modouaichadiop@gmail.com>
2025-02-26 15:38:07 +0000

    update

--- mistral/clean.py
+++ mistral/clean.py
@@ -24,8 +24,11 @@ def extract_coordinates(geometry):
         return None
 
 # Extraire les coordonnées
-df_filtered['lon'] = df_filtered['geometry.type'].apply(lambda x: extract_coordinates({'type': x, 'coordinates': df_filtered.loc[df_filtered['geometry.type'] == x, 'geometry.coordinates'].values[0]})[0] if extract_coordinates({'type': x, 'coordinates': df_filtered.loc[df_filtered['geometry.type'] == x, 'geometry.coordinates'].values[0]}) else None)
-df_filtered['lat'] = df_filtered['geometry.type'].apply(lambda x: extract_coordinates({'type': x, 'coordinates': df_filtered.loc[df_filtered['geometry.type'] == x, 'geometry.coordinates'].values[0]})[1] if extract_coordinates({'type': x, 'coordinates': df_filtered.loc[df_filtered['geometry.type'] == x, 'geometry.coordinates'].values[0]}) else None)
+df_filtered['lon'] = df_filtered['geometry'].apply(lambda x: extract_coordinates(x)[0] if extract_coordinates(x) else None)
+df_filtered['lat'] = df_filtered['geometry'].apply(lambda x: extract_coordinates(x)[1] if extract_coordinates(x) else None)
+
+# Supprimer les lignes avec des coordonnées manquantes
+df_filtered.dropna(subset=['lat', 'lon'], inplace=True)
 
 # Afficher les premières lignes pour vérifier
 print(df_filtered[['properties.highway', 'lat', 'lon']].head())
@@ -35,8 +38,7 @@ map_viz = folium.Map(location=[48.4541, -2.0474], zoom_start=13)
 
 # Ajouter les points d'intérêt à la carte
 for idx, row in df_filtered.iterrows():
-    if row['lat'] is not None and row['lon'] is not None:
-        folium.Marker(location=[row['lat'], row['lon']], popup=row['properties.highway']).add_to(map_viz)
+    folium.Marker(location=[row['lat'], row['lon']], popup=row['properties.highway']).add_to(map_viz)
 
 # Sauvegarder la carte
 map_viz.save("./mistral/carte_dinan.html")
